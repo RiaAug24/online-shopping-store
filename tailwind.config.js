77167b2ee99dda466d0e1791a258ad463ffb4670
Riyaz Ahmed <riyaz04ahmed24aug.github@gmail.com>
2024-08-25 21:40:28 +0530

    Setup initial project requirements

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './views/**/*.ejs',  // This will include all .ejs files in the views directory and its subdirectories
+    './public/styles/*.css',  // Optional: Include your CSS files if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
